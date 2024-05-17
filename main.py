b3447c53ddecc7b5e18b1d4282cea62c5859ce1a
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-05-17 20:42:53 +0400

    basic matrix test drawing

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import pygame
 
 pygame.init()
 
+# matrix drawings preparations
+
+matrix_edge_range = 20
+
+cell_edge_dimension = 20
+gap_bw_cells = cell_edge_dimension // 5
+
 # screen initialization
-screen_width = 600
-screen_height = 600
+screen_width = cell_edge_dimension * matrix_edge_range + gap_bw_cells * (matrix_edge_range + 1)
+screen_height = cell_edge_dimension * matrix_edge_range + gap_bw_cells * (matrix_edge_range + 1)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# drawings initialization
-Rec1 = pygame.Rect((100, 100, 50, 50))
+# drawings|colors initialization
+# Rec1 = pygame.Rect((100, 0, 50, 50))
 
-RGB_WHITE = (255, 255, 255)
-RGB_RED = (255, 0, 0)
+COLOR_RGB_BLACK = (0, 0, 0)
+COLOR_RGB_WHITE = (255, 255, 255)
+COLOR_RGB_RED = (255, 0, 0)
+COLOR_RGB_GREEN = (0, 255, 0)
+COLOR_RGB_BLUE = (0, 0, 255)
 
 # matrix initialization - to store values
-matrix_edge_range = 5
+
 matrix = [[0 for j in range(matrix_edge_range)] for i in range(matrix_edge_range)]
 
 for i in matrix:
@@ -23,20 +33,26 @@ for i in matrix:
     print()
 
 # matrix for drawing the display, based on values matrix cols|rows
-drawing_coordinates_matrix = [[(0, 0) for j in i] for i in matrix]
-# print(len(drawing_coordinates_matrix))
+drawing_coordinates_matrix = [[j for j in i] for i in matrix]
 
-for i in range(len(drawing_coordinates_matrix)):
-    for j in range(len(drawing_coordinates_matrix[j])):
-        print(j, end='')
-    print()
+# fill drawing coordinates (start of Rec's)
+for i in range(matrix_edge_range):
+    for j in range(matrix_edge_range):
+        drawing_coordinates_matrix[i][j] = pygame.Rect((i * (cell_edge_dimension + gap_bw_cells) + gap_bw_cells,
+                                                        j * (cell_edge_dimension + gap_bw_cells) + gap_bw_cells,
+                                                        cell_edge_dimension, cell_edge_dimension))
+    # print()
 
 # main loop
 run = True
 
 while run:
 
-    screen.fill(RGB_WHITE)
+    screen.fill(COLOR_RGB_BLACK)
+
+    for line in drawing_coordinates_matrix:
+        for pos in line:
+            pygame.draw.rect(screen, COLOR_RGB_WHITE, pos)
 
     # event handler
     for event in pygame.event.get():
