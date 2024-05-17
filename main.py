e9480685650d51677e2ef8920072dd3cf2490099
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-05-17 19:51:46 +0400

    import pygame testest

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pygame
 
-print('hello!')
+pygame.init()
+
+screen_width = 600
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+run = True
+
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
