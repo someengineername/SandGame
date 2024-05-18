7a8c2654d696655b27296f6dfd0027c32557395b
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-05-18 19:25:06 +0400

    falling mech. done 100%, w\0 bordere checking - still with bugs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 # colors initialization
 COLOR_RGB_BLACK = (0, 0, 0)
@@ -45,7 +46,7 @@ def is_bottom_position_sand(matrix, coor_x, coor_y):
 
 # matrix edge dimensions preparations
 matrix_dimension = 50
-cell_length = 10
+cell_length = 5
 gap_bw_cells = cell_length // 10
 
 # screen initialization with update to cell size
@@ -55,12 +56,6 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 # matrix initialization - to store values
 matrix = [[False for j in range(matrix_dimension)] for i in range(matrix_dimension)]
-#
-# for i in range(matrix_dimension - 1, -1, -1):
-#     for j in range(matrix_dimension -1 , -1, -1):
-#         print( [i, j], end='')
-#     print()
-
 
 # main loop
 run = True
@@ -69,31 +64,56 @@ while run:
 
     mouse_coord = []
 
+    # -------------
     # event handler
+    # -------------
     for event in pygame.event.get():
 
         # different variations of events goes here...
         if event.type == pygame.QUIT:
             run = False
 
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_coord = pygame.mouse.get_pos()
 
             coordinates_of_click = [mouse_coord[0] // (cell_length + gap_bw_cells),
                                     mouse_coord[1] // (cell_length + gap_bw_cells)]
             reverse_flag(matrix, coordinates_of_click[0], coordinates_of_click[1])
 
-    # matrix re-fillement section
+    # -------------
+    # matrix-for-drawing preparation
+    # -------------
+    temp_matrix = [[False for j in range(matrix_dimension)] for i in range(matrix_dimension)]
 
+    # matrix re-filament section ( check value in matrix and copy it
     for i in range(matrix_dimension - 1, -1, -1):
         for j in range(matrix_dimension - 1, -1, -1):
             if matrix[i][j] is True:
                 if j == matrix_dimension - 1:
-                    pass
+                    temp_matrix[i][j] = True
                 else:
+                    # if bottom is empty
                     if matrix[i][j + 1] is False:
-                        matrix[i][j] = False
-                        matrix[i][j + 1] = True
+                        temp_matrix[i][j] = False
+                        temp_matrix[i][j + 1] = True
+                    # if bottom is occupied
+                    else:
+
+                        # if right side occupied
+                        if matrix[i - 1][j + 1] is False and matrix[i + 1][j + 1] is True:
+                            temp_matrix[i - 1][j + 1] = True
+                        # if left side occupied
+                        elif matrix[i + 1][j + 1] is False and matrix[i - 1][j + 1] is True:
+                            temp_matrix[i + 1][j + 1] = True
+                        # if both sides occupied - 50|50 %
+                        elif matrix[i + 1][j + 1] is False and matrix[i - 1][j + 1] is False:
+                            if random.randint(0, 1) == 1:
+                                temp_matrix[i - 1][j + 1] = True
+                            else:
+                                temp_matrix[i + 1][j + 1] = True
+                        # if everything's occupied
+                        elif matrix[i + 1][j + 1] is True and matrix[i - 1][j + 1] is True and matrix[i][j + 1] is True:
+                            temp_matrix[i][j] = True
             else:
                 pass
 
@@ -102,22 +122,21 @@ while run:
     # -------------
 
     # drawing black canvas
-    screen.fill(COLOR_RGB_BLACK)
+    screen.fill(COLOR_RGB_WHITE)
 
     # drawing rectangles according to validity (TRUE | FALSE)
     for i in range(matrix_dimension):
         for j in range(matrix_dimension):
-            pygame.draw.rect(screen, return_color_for_cell(matrix, i, j),
+            pygame.draw.rect(screen, return_color_for_cell(temp_matrix, i, j),
                              (i * (cell_length + gap_bw_cells) + gap_bw_cells,
                               j * (cell_length + gap_bw_cells) + gap_bw_cells,
                               cell_length, cell_length))
 
-    # refrash screen and draw everything
+    # -------------
+    # frame-generation-like
+    # -------------
     tick(0.05)
 
-    # new_temp_matrix = [[False for j in range(matrix_dimension)] in range(matrix_dimension)]
-
-    # for i in range(matrix_dimension):
-    #     for j in range(matrix_dimension):
+    matrix = temp_matrix
 
 pygame.quit()
