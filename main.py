b0049f35789d40ef6be3e8c44b0cfcf7800df68d
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-05-18 16:21:50 +0400

    simple falling mechanism implemented - without 50\50 chance to fall on sides

--- main.py
+++ main.py
@@ -31,6 +31,18 @@ def reverse_flag(matrix, coor_x, coor_y):
         matrix[coor_x][coor_y] = True
 
 
+def is_actual_position_sand(matrix, coor_x, coor_y):
+    if matrix[coor_x][coor_y] is False:
+        return False
+    return True
+
+
+def is_bottom_position_sand(matrix, coor_x, coor_y):
+    if matrix[coor_x + 1][coor_y] is False:
+        return False
+    return True
+
+
 # matrix edge dimensions preparations
 matrix_dimension = 50
 cell_length = 10
@@ -43,6 +55,12 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 # matrix initialization - to store values
 matrix = [[False for j in range(matrix_dimension)] for i in range(matrix_dimension)]
+#
+# for i in range(matrix_dimension - 1, -1, -1):
+#     for j in range(matrix_dimension -1 , -1, -1):
+#         print( [i, j], end='')
+#     print()
+
 
 # main loop
 run = True
@@ -61,14 +79,32 @@ while run:
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_coord = pygame.mouse.get_pos()
 
+            coordinates_of_click = [mouse_coord[0] // (cell_length + gap_bw_cells),
+                                    mouse_coord[1] // (cell_length + gap_bw_cells)]
+            reverse_flag(matrix, coordinates_of_click[0], coordinates_of_click[1])
+
+    # matrix re-fillement section
+
+    for i in range(matrix_dimension - 1, -1, -1):
+        for j in range(matrix_dimension - 1, -1, -1):
+            if matrix[i][j] is True:
+                if j == matrix_dimension - 1:
+                    pass
+                else:
+                    if matrix[i][j + 1] is False:
+                        matrix[i][j] = False
+                        matrix[i][j + 1] = True
+            else:
+                pass
+
+    # -------------
     # screen drawing section
+    # -------------
+
+    # drawing black canvas
     screen.fill(COLOR_RGB_BLACK)
 
-    if mouse_coord:
-        coordinates_of_click = [mouse_coord[0] // (cell_length + gap_bw_cells),
-                                mouse_coord[1] // (cell_length + gap_bw_cells)]
-        reverse_flag(matrix, coordinates_of_click[0], coordinates_of_click[1])
-
+    # drawing rectangles according to validity (TRUE | FALSE)
     for i in range(matrix_dimension):
         for j in range(matrix_dimension):
             pygame.draw.rect(screen, return_color_for_cell(matrix, i, j),
@@ -76,6 +112,12 @@ while run:
                               j * (cell_length + gap_bw_cells) + gap_bw_cells,
                               cell_length, cell_length))
 
-    tick(0.001)
+    # refrash screen and draw everything
+    tick(0.05)
+
+    # new_temp_matrix = [[False for j in range(matrix_dimension)] in range(matrix_dimension)]
+
+    # for i in range(matrix_dimension):
+    #     for j in range(matrix_dimension):
 
 pygame.quit()
